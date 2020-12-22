a6b41dfd1fb4dcf00cd5f3362ab90267ce887768
Dima Voichuck <dvoichuck@e1r6p5.unit.ua>
2020-12-22 15:38:28 +0200

    libmx

--- client.c
+++ client.c
@@ -21,14 +21,13 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     char c;
 
-    if(argc != 2) {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
+    if (argc != 2) {
+        printf("\n Usage: %s <ip of server> \n", argv[0]);
         return 1;
     }
 
-    memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    memset(recvBuff, '0', sizeof(recvBuff));
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Error : Could not create socket \n");
         return 1;
     }
@@ -38,30 +37,21 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000);
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-    {
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
         printf("\n inet_pton error occured\n");
         return 1;
     }
-
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sockfd, (struct sockaddr *) &serv_addr,
+                sizeof(serv_addr)) <
+        0) {
         printf("\n Error : Connect Failed \n");
         return 1;
     }
-
-    scanf("%s", str);
-    send(sockfd, str, strlen(str), MSG_OOB);
-    fp = fdopen(sockfd, "r");
-    while ((c = fgetc(fp)) > 0)
-        printf("%c", c);
-
-
-
-    if(n < 0)
-    {
-        printf("\n Read error \n");
+    if ((fp = fdopen(sockfd, "r")) > 0) {
+        while ((c = fgetc(fp)) > 0)
+            printf("%c", c);
     }
+    send(sockfd, "\nlol", 3, MSG_OOB);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -12,13 +12,15 @@
 #include <sys/types.h>
 #include <time.h>
 
+
 int main (int argc, char *argv[]) {
     int listen_fd = 0;
     int conn_fd = 0;
     struct sockaddr_in serv_addr;
     char sendBuff[1025];
-    time_t ticks;
+    char *str;
     FILE *fp;
+    char ch;
 
     listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
@@ -33,13 +35,9 @@ int main (int argc, char *argv[]) {
     listen(listen_fd, 10);
     while (1) {
         conn_fd = accept(listen_fd, (struct sockaddr *) NULL, NULL);
-        send(conn_fd, "Enter text:", 7, 0);
-
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-        write(conn_fd, sendBuff, strlen(sendBuff));
-
-        close(conn_fd);
+        send(conn_fd, "Enter text:", 12, 0);
+        if (recv(conn_fd, sendBuff, sizeof(sendBuff), MSG_PEEK))
+            printf("%s\n", sendBuff);
         sleep(1);
     }
 }
