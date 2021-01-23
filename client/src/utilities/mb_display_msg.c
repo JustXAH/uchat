4d9f090ddebb48a1618a390d7b3d2f7cb353f453
mbodak <maksym.bodak@gmail.com>
2021-01-24 00:41:43 +0200

    basic window switching working + updated structures for chats&contacts lists

--- client/src/utilities/mb_display_msg.c
+++ client/src/utilities/mb_display_msg.c
@@ -1,21 +1,23 @@
 #include "client.h"
 
 extern t_chat_win chat_win;
+extern t_client_st cl_listener;
 
 void mb_display_msg(t_message *msg) {
+    //mx_printstr("mb_display_msg");
     char *txt_msg;
     char *txt_buff;
     GtkWidget *view;
-    GtkTextBuffer *buffer;
 
-    //txt_buff = mx_strjoin(msg->timestamp, msg->user);
-    txt_buff = mx_itoa(msg->user);
+    txt_buff = mx_strjoin(msg->user_name, ": ");
     txt_msg = mx_strjoin(txt_buff, msg->text);
-    view = gtk_text_view_new();
-    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
-    gtk_text_buffer_set_text(buffer, txt_msg, -1);
-    gtk_container_add(GTK_CONTAINER(chat_win.chat_viewer), view);
+
+    view = gtk_label_new(txt_msg);
+    gtk_label_set_xalign(GTK_LABEL(view), 0);
+
+    gtk_container_add(GTK_CONTAINER(chat_win.msg_viewer), view);
     gtk_widget_show(view);
+
     free(txt_msg);
     free(txt_buff);
 }
