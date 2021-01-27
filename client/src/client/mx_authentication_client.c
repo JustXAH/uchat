5ed319f464b44dd7e96e2c9cc755c822adb3dc1f
maxBodak <72943265+maxBodak@users.noreply.github.com>
2021-01-27 23:34:11 +0200

    Revert "..."
    This reverts commit 5a017860d58bbcf20d61cf1c2223857b3e528a44.

--- client/src/client/mx_authentication_client.c
+++ client/src/client/mx_authentication_client.c
@@ -83,7 +83,6 @@ void mx_authentication_client(t_system *sys, t_user *user, t_json *json) {
         cl_listener.my_id = json->USER_ID->valueint;
         
         for (int i = 0; i != user->contacts_count; i++) {
-            mx_printstr("adding contact");
             mb_contact_list_add(user->chats_id[i], user->contacts_id[i], user->contacts_login[i], false);
         }
     }
--- server/src/mx_add_new_message.c
+++ server/src/mx_add_new_message.c
@@ -15,7 +15,6 @@ static void which_user_online(t_server *serv, t_json *json, char *send_str) {
 void mx_add_new_message(t_server *serv, t_json *json, int user_index) {
     char *send_str = NULL;
 
-
     json->USER_ID = cJSON_GetObjectItemCaseSensitive(json->USER_JSON,"USER_ID");
     json->CHAT_ID = cJSON_GetObjectItemCaseSensitive(json->USER_JSON,"CHAT_ID");
     json->MESSAGE = cJSON_GetObjectItemCaseSensitive(json->USER_JSON,"MESSAGE");
@@ -31,11 +30,8 @@ void mx_add_new_message(t_server *serv, t_json *json, int user_index) {
     
     t_message_info *message_info = mx_db_get_message(serv->db, json->MESSAGE_ID->valueint);
 
-    cJSON *USER_ID = cJSON_CreateNumber(message_info->user);
-    cJSON *CHAT_ID = cJSON_CreateNumber(json->CHAT_ID->valueint);
-    cJSON *MESSAGE_ID = cJSON_CreateNumber(message_info->id);
-    cJSON *MESSAGE = cJSON_CreateString(message_info->text);
-    cJSON *MESSAGE_TIME =  cJSON_CreateNumber(message_info->timestamp);;
+    json->MESSAGE_TIME = cJSON_CreateNumber(message_info->timestamp);
+    // json->
 
     if (json->MESSAGE_ID->valueint == 0) {
         json->RESULT = cJSON_CreateFalse();
@@ -43,11 +39,10 @@ void mx_add_new_message(t_server *serv, t_json *json, int user_index) {
     }
     else {
         json->RESULT = cJSON_CreateTrue();
-        cJSON_AddItemToObject(json->SEND, "MESSAGE_TIME", MESSAGE_TIME);
-        cJSON_AddItemToObject(json->SEND, "MESSAGE_ID", MESSAGE_ID);
-        cJSON_AddItemToObject(json->SEND, "MESSAGE", MESSAGE);
-        cJSON_AddItemToObject(json->SEND, "CHAT_ID", CHAT_ID);
-        cJSON_AddItemToObject(json->SEND, "USER_ID", USER_ID);
+        cJSON_AddItemToObject(json->SEND, "MESSAGE_ID", json->MESSAGE_ID);
+        cJSON_AddItemToObject(json->SEND, "MESSAGE", json->MESSAGE_ID);
+        cJSON_AddItemToObject(json->SEND, "CHAT_ID", json->CHAT_ID);
+        cJSON_AddItemToObject(json->SEND, "CONTACT_ID", json->CONTACT_ID);
     }
 
     send_str = cJSON_Print(json->SEND);
