ca46f9afb6f5e6055af8f8d74a908d6114ab4626
dvoichuck <69978760+dvoichuck@users.noreply.github.com>
2021-01-28 23:00:29 +0200

    reverted db_get_chats_info to prev working version

--- client/src/handlers/on_friends_search_entry_changed.c
+++ client/src/handlers/on_friends_search_entry_changed.c
@@ -7,7 +7,7 @@ extern t_chat *chat;
 
 static void get_search_results();
 void on_friends_search_entry_changed() {
-    printf("entry search\n");
+    printf("entry search started\n");
     char *query = (char *)gtk_entry_get_text(GTK_ENTRY(chat_win.fsearch_entry));
     t_chat_list *con_buf = contact_list;
 
@@ -34,6 +34,7 @@ void on_friends_search_entry_changed() {
         get_search_results();
         cl_listener.fsearch = true;
     } else { //User stopped searching
+        mx_printstr("Clearing search results\n");
         gtk_stack_set_visible_child(chat_win.search_stack, 
                                     gtk_widget_get_parent(
                                         gtk_widget_get_parent(
@@ -41,6 +42,7 @@ void on_friends_search_entry_changed() {
 
         cl_listener.fsearch = false;
     }
+    printf("entry search finished\n");
 }
 static void get_search_results() {
     while (cl_listener.pending_requests[USER_SEARCH_BY_SUBSTRING]); //waiting to receive results
